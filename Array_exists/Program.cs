4cc5013d62fceda852b4c9bd0b71ad69bc6f8c59
Joyonta <joyonta080@gmail.com>
2026-09-12 16:35:20 +0530

    Add project files.

---
+++ Array_exists/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Array_exists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            bool result = Array.Exists(numbers, x => x > 50);
+            Console.WriteLine(result);
+            
+        }
+    }
+}
