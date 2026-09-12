16028f311f5191bbaebefeb88a37ae40f753ca41
Joyonta <joyonta080@gmail.com>
2026-09-12 16:38:33 +0530

    add comment

--- Array_exists/Program.cs
+++ Array_exists/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             int[] numbers = { 10, 20, 30, 40, 50 };
-            bool result = Array.Exists(numbers, x => x > 50);
+            bool result = Array.Exists(numbers, x => x > 30); //Lambda Expression: x => x > 30
             Console.WriteLine(result);
+
+
+
+              /* x = 10 → 10 > 30 → False ❌
+
+               x = 20 → 20 > 30 → False ❌
+
+              x = 30 → 30 > 30 → False ❌
+
+              x = 40 → 40 > 30 → True ✅ */
             
         }
     }
